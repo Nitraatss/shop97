139b7d5ba81b2492d4ce4fb33aa808761a17d950
Vasilii <nitraatss@gmail.com>
2019-06-18 10:42:47 +0300

    Изменено переключение вкладок меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,13 +3,7 @@ const initiateMenu = () => {
   const togglers = [].slice.call(
       mainHeader.querySelectorAll(`.main-header__toggler`)
   );
-  const maleToggler = mainHeader.querySelector(`.main-header__toggler--male`);
-  const femaleToggler = mainHeader.querySelector(
-      `.main-header__toggler--female`
-  );
   const navigations = [].slice.call(mainHeader.querySelectorAll(`.navigation`));
-  const maleNavigation = mainHeader.querySelector(`.navigation--male`);
-  const femaleNavigation = mainHeader.querySelector(`.navigation--female`);
   const mainHeaderTop = mainHeader.querySelector(`.main-header__top`);
   const burger = mainHeader.querySelector(`.main-header__burger`);
 
@@ -23,42 +17,22 @@ const initiateMenu = () => {
     });
   };
 
-  const onTogglerClick = (
-      toggler,
-      navigation,
-      secondaryToggler,
-      secondaryNavigation
-  ) => {
-    if (toggler.classList.value.includes(`main-header__toggler--active`)) {
-      toggler.classList.remove(`main-header__toggler--active`);
-      navigation.classList.remove(`navigation--active`);
-    } else {
-      toggler.classList.add(`main-header__toggler--active`);
-      navigation.classList.add(`navigation--active`);
-      secondaryToggler.classList.remove(`main-header__toggler--active`);
-      secondaryNavigation.classList.remove(`navigation--active`);
-    }
+  togglers.forEach((toggler, index) => {
+    toggler.addEventListener(`click`, () => {
+      if (!toggler.classList.value.includes(`main-header__toggler--active`)) {
+        hideNavigations();
 
-    burger.classList.remove(`main-header__burger--active`);
-    mainHeaderTop.classList.remove(`main-header__top--active`);
-  };
+        toggler.classList.add(`main-header__toggler--active`);
+        navigations[index].classList.add(`navigation--active`);
+      } else {
+        hideNavigations();
+      }
 
-  maleToggler.addEventListener(`click`, () => {
-    onTogglerClick(
-        maleToggler,
-        maleNavigation,
-        femaleToggler,
-        femaleNavigation
-    );
+      burger.classList.remove(`main-header__burger--active`);
+      mainHeaderTop.classList.remove(`main-header__top--active`);
+    });
   });
-  femaleToggler.addEventListener(`click`, () => {
-    onTogglerClick(
-        femaleToggler,
-        femaleNavigation,
-        maleToggler,
-        maleNavigation
-    );
-  });
+
   burger.addEventListener(`click`, () => {
     burger.classList.toggle(`main-header__burger--active`);
     mainHeaderTop.classList.toggle(`main-header__top--active`);
@@ -67,9 +41,10 @@ const initiateMenu = () => {
   });
 
   window.addEventListener(`resize`, () => {
-    hideNavigations();
     burger.classList.remove(`main-header__burger--active`);
     mainHeaderTop.classList.remove(`main-header__top--active`);
+
+    hideNavigations();
   });
 };
 
